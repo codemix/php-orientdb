c8f9faaed894cf5ab7cc17adb8785e83f0a41174
Charles Pick <charles.pick@gmail.com>
2014-05-06 20:47:38 +0100

    add custom query classes

---
+++ src/Orienta/Query/AbstractQuery.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Orienta\Query;
+
+use Orienta\Common\ConfigurableInterface;
+use Orienta\Common\ConfigurableTrait;
+use Orienta\Record\Serializer;
+
+abstract class AbstractQuery implements QueryInterface, ConfigurableInterface
+{
+    use ConfigurableTrait;
+
+    /**
+     * @var string The query text.
+     */
+    public $text = '';
+
+    /**
+     * @var array The parameters for the query.
+     */
+    public $params = [];
+
+    /**
+     * Serialize the parameters for the query.
+     *
+     * > Note: There is a bug in OrientDB where special kinds of string values
+     * > need to be twice quoted *in parameters*. Hence the need for this specialist function.
+     *
+     * @return string The serialized parameters.
+     */
+    protected function serializeParams()
+    {
+        $collected = [];
+        foreach($this->params as $key => $value) {
+            if (is_string($value) && strlen($value)) {
+                $c = $value[0];
+                $isQuotable = (
+                    $c === '#'
+                    || $c === '<'
+                    || $c === '['
+                    || $c === '('
+                    || $c === '{'
+                    || $c === '0'
+                    || is_numeric($c)
+                );
+                if ($isQuotable) {
+                    $collected[$key] = '"'.$value.'"';
+                }
+                else {
+                    $collected[$key] = $value;
+                }
+            }
+        }
+        return Serializer::serialize(['params' => $collected]);
+    }
+
+}
---
+++ src/Orienta/Query/Sync.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Orienta\Query;
+
+
+use Orienta\Common\Binary;
+
+/**
+ * # Synchronous Query
+ *
+ * @package Orienta\Query
+ */
+class Sync extends AbstractQuery
+{
+
+    /**
+     * @var int The non-text limit for the query.
+     */
+    public $limit = -1;
+
+    /**
+     * @var string The fetch plan for the query.
+     */
+    public $fetchPlan = '';
+
+    /**
+     * Get the name or alias of the remote OrientDB query class that this class represents.
+     * @return string
+     */
+    public function getOrientClass()
+    {
+        return 'q';
+    }
+
+    /**
+     * Get a binary representation of the query, ready to send to orient.
+     *
+     * @return string A binary serialized version of the query class.
+     */
+    public function binarySerialize()
+    {
+        $bytes = Binary::packString($this->getOrientClass());
+        $bytes .= Binary::packString($this->text);
+        $bytes .= Binary::packInt($this->limit);
+        $bytes .= Binary::packString($this->fetchPlan);
+
+        if (count($this->params)) {
+            $bytes .= Binary::packString($this->serializeParams());
+        }
+        else {
+            $bytes .= Binary::packInt(0);
+        }
+        return $bytes;
+    }
+
+}
