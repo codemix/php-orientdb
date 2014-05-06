5de39ac1351af48ec7d5add7565bbca0840124ac
Charles Pick <charles.pick@gmail.com>
2014-05-06 01:50:23 +0100

    add common binary / math functions

--- src/Oriento/Protocols/Binary/Operations/AbstractOperation.php
+++ src/Oriento/Protocols/Binary/Operations/AbstractOperation.php
@@ -2,8 +2,10 @@
 
 namespace Oriento\Protocols\Binary\Operations;
 
+use Oriento\Common\Binary;
 use Oriento\Common\ConfigurableInterface;
 use Oriento\Common\ConfigurableTrait;
+use Oriento\Common\Math;
 use Oriento\Exceptions\Exception;
 use Oriento\Protocols\Binary\Socket;
 
@@ -88,7 +90,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeByte($value)
     {
-        $this->socket->write(chr($value));
+        $this->socket->write(Binary::packByte($value));
     }
 
     /**
@@ -99,7 +101,7 @@ abstract class AbstractOperation implements ConfigurableInterface
     protected function readByte()
     {
         $value = $this->socket->read(1);
-        return $value;
+        return Binary::unpackByte($value);
     }
 
     /**
@@ -109,7 +111,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeShort($value)
     {
-        $this->socket->write(pack('n', $value));
+        $this->socket->write(Binary::packShort($value));
     }
 
     /**
@@ -119,7 +121,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function readShort()
     {
-        return $this->convertComplementShort(unpack('n', $this->socket->read(2))[1]);
+        return Binary::unpackShort($this->socket->read(2));
     }
 
     /**
@@ -129,7 +131,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeInt($value)
     {
-        $this->socket->write(pack('N', $value));
+        $this->socket->write(Binary::packInt($value));
     }
 
     /**
@@ -139,8 +141,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function readInt()
     {
-        $value = unpack('N', $this->socket->read(4));
-        return $this->convertComplementInt(reset($value));
+        return Binary::unpackInt($this->socket->read(4));
     }
 
 
@@ -151,7 +152,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeLong($value)
     {
-        $this->socket->write(str_repeat(chr(0), 4).pack('N', $value));
+        $this->socket->write(Binary::packLong($value));
     }
 
     /**
@@ -161,13 +162,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function readLong()
     {
-        // First of all, read 8 bytes, divided into hi and low parts
-        $hi = unpack('N', $this->socket->read(4));
-        $hi = reset($hi);
-        $low = unpack('N', $this->socket->read(4));
-        $low = reset($low);
-        // Unpack 64-bit signed long
-        return $this->unpackI64($hi, $low);
+        return Binary::unpackLong($this->socket->read(8));
     }
 
     /**
@@ -177,8 +172,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeString($value)
     {
-        $this->writeInt(strlen($value));
-        $this->socket->write($value);
+        $this->socket->write(Binary::packString($value));
     }
 
     /**
@@ -207,8 +201,7 @@ abstract class AbstractOperation implements ConfigurableInterface
      */
     protected function writeBytes($value)
     {
-        $this->writeInt(strlen($value));
-        $this->socket->write($value);
+        $this->socket->write(Binary::packBytes($value));
     }
 
     /**
@@ -249,92 +242,5 @@ abstract class AbstractOperation implements ConfigurableInterface
         return new Exception($type.': '.$message);
     }
 
-    /**
-     * Convert twos-complement integer after unpack() on x64 systems.
-     *
-     * @param int $int The integer to convert.
-     * @return int The converted integer.
-     */
-    protected function convertComplementInt($int)
-    {
-        /*
-         *  Valid 32-bit signed integer is -2147483648 <= x <= 2147483647
-         *  -2^(n-1) < x < 2^(n-1) -1 where n = 32
-         */
-        if ($int > 2147483647) {
-            return -(($int ^ 0xFFFFFFFF) + 1);
-        }
-        return $int;
-    }
 
-    /**
-     * Convert twos-complement short after unpack() on x64 systems.
-     *
-     * @param int $short The short to convert.
-     * @return int The converted short.
-     */
-    protected function convertComplementShort($short)
-    {
-        /*
-         *  Valid 16-bit signed integer is -32768 <= x <= 32767
-         *  -2^(n-1) < x < 2^(n-1) -1 where n = 16
-         */
-        if ($short > 32767) {
-            return -(($short ^ 0xFFFF) + 1);
-        }
-        return $short;
-    }
-
-    /**
-     * Unpacks 64 bits signed long
-     *
-     * @param $hi int Hi bytes of long
-     * @param $low int Low bytes of long
-     * @return int|string
-     */
-    protected function unpackI64($hi, $low)
-    {
-        // Packing is:
-        // OrientDBHelpers::hexDump(pack('NN', $int >> 32, $int & 0xFFFFFFFF));
-
-        // If x64 system, just shift hi bytes to the left, add low bytes. Piece of cake.
-        if (PHP_INT_SIZE === 8) {
-            return ($hi << 32) + $low;
-        }
-
-        // x32
-        // Check if long could fit into int
-        $hiComplement = self::convertComplementInt($hi);
-        if ($hiComplement === 0) {
-            // Hi part is 0, low will fit in x32 int
-            return $low;
-        } elseif ($hiComplement === -1) {
-            // Hi part is negative, so we just can convert low part
-            if ($low >= 0x80000000) {
-                // Check if low part is lesser than minimum 32 bit signed integer
-                return self::convertComplementInt($low);
-            }
-        }
-
-        // Sign char
-        $sign = '';
-        $lastBit = 0;
-        // This is negative number
-        if ($hiComplement < 0) {
-            $hi = ~$hi;
-            $low = ~$low;
-            $lastBit = 1;
-            $sign = '-';
-        }
-
-        // Format bytes properly
-        $hi = sprintf('%u', $hi);
-        $low = sprintf('%u', $low);
-
-        // Do math
-        $temp = bcmul($hi, '4294967296');
-        $temp = bcadd($low, $temp);
-        $temp = bcadd($temp, $lastBit);
-        return $sign . $temp;
-    }
 }
