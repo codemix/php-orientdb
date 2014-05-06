7247f0d8061e3c59dac72272cc817efb2583fccd
Charles Pick <charles.pick@gmail.com>
2014-05-06 12:19:27 +0100

    split binary specific tests

--- tests/Oriento/ClientTest.php
+++ tests/Oriento/ClientTest.php
@@ -2,94 +2,13 @@
 
 namespace Oriento;
 
-class ClientTest extends TestCase
+class ClientTest extends DbTestCase
 {
-    public function testConfigure()
+
+    public function testGetDatabases()
     {
-        $client = new Client();
-        $client->configure([
-            'port' => 2424,
-            'transport' => 'binary'
-        ]);
-        $transport = $client->getTransport();
-        $this->assertInstanceOf('Oriento\\Protocols\\Binary\\Transport', $transport);
-    }
-
-    /**
-     * @expectedException \Oriento\Exceptions\Exception
-     */
-    public function testConnectWithBadCredentials()
-    {
-        $client = $this->createClient();
-        $sessionId = $client->execute('connect', [
-            'username' => $client->username,
-            'password' => 'thisisnottherightpassword!'
-        ]);
-
-        $this->assertGreaterThan(0, $sessionId);
-    }
-
-    public function testConnect()
-    {
-        $client = $this->createClient();
-        $sessionId = $client->execute('connect', [
-            'username' => $client->username,
-            'password' => $client->password
-        ]);
-
-        $this->assertGreaterThan(0, $sessionId);
-    }
-
-    public function testDbList()
-    {
-        $client = $this->createClient();
-        $sessionId = $client->execute('connect', [
-            'username' => $client->username,
-            'password' => $client->password
-        ]);
-
-        $result = $client->execute('dbList', [
-           'sessionId' => $sessionId
-        ]);
-
-        $this->assertArrayHasKey('databases', $result);
-        $this->assertGreaterThan(0, count($result['databases']));
-    }
-
-    public function testDbExists()
-    {
-        $client = $this->createClient();
-        $sessionId = $client->execute('connect', [
-            'username' => $client->username,
-            'password' => $client->password
-        ]);
-
-        $result = $client->execute('dbExists', [
-            'sessionId' => $sessionId,
-            'database' => 'GratefulDeadConcerts'
-        ]);
-
-        $this->assertTrue($result);
-
-        $result = $client->execute('dbExists', [
-            'sessionId' => $sessionId,
-            'database' => 'missing_db'
-        ]);
-
-        $this->assertFalse($result);
-    }
-
-    public function testDbOpen()
-    {
-        $client = $this->createClient();
-        $result = $client->execute('dbOpen', [
-            'database' => 'GratefulDeadConcerts',
-            'type' => 'graph',
-            'username' => 'admin',
-            'password' => 'admin',
-        ]);
-
-        $this->assertArrayHasKey('clusters', $result);
-        $this->assertGreaterThan(5, count($result['clusters']));
+        $dbs = $this->client->getDatabases();
+        $this->assertArrayHasKey('oriento_clienttest', $dbs);
+        $this->assertInstanceOf('Oriento\\Database\\Database', $dbs['oriento_clienttest']);
     }
 }
